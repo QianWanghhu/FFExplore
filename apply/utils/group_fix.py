ffe3f82b2a237ff08b3e68318efdddfd6d56adab
Qian Wang <qian@hhu.edu.cn>
2020-06-26 10:07:46 +1000

    Add to_df func

--- apply/utils/group_fix.py
+++ apply/utils/group_fix.py
@@ -66,3 +66,37 @@ def group_fix(partial_result, func, x, y_true, x_default, a=None, file_exist=Fal
         compare_mae[i] = (np.abs(results_fix - y_true) / y_true_ave).mean(axis=0)
         # pearsons[i] = 1 - (((results_fix - y_true) **2).sum(axis=0) / y_true_var / len(y_true))
     return compare_mean, compare_mae, compare_var, pearsons
+
+def to_df(partial_order, fix_dict, y_true):
+    """
+    Help function to convert difference between 
+    conditioned and unconditioned into dataframe.
+    Parameters:
+    ===========
+    partial_order : dict, partial ranking of parameters
+    fix_dict : dict, difference between conditioned and unconditional model results.
+                (each dict result returned from group_fix / pce_group_fix)
+    y_true : numpy.ndarray, unconditional model results
+
+    Returns:
+    ========
+    fix_df : df, formatted fix_dict
+    """
+    keys_list = list(partial_order.keys())
+
+    fix_df = {key:None for key in keys_list}
+
+    for key in keys_list:
+        len_each_group = []
+        len_each_group = [len(value) for k, value in partial_order[key].items()]
+        fix_temp = []
+
+        for g, v in fix_dict[key].items():
+            if isinstance(v, tuple):
+                fix_temp.extend([v[0]])
+            else:
+                fix_temp.extend([v])
+        fix_df[key]  = np.repeat(fix_temp, len_each_group)
+
+    fix_df = pd.DataFrame.from_dict(fix_df)
+    return fix_df
