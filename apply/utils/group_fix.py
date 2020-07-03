f1b8e5cb93f7f13e84fa0dbc0a076d34c0abfcb2
Qian Wang <qian@hhu.edu.cn>
2020-07-03 22:39:02 +1000

    rename variables

--- apply/utils/group_fix.py
+++ apply/utils/group_fix.py
@@ -1,7 +1,8 @@
 import numpy as np
-from scipy.stats import pearsonr
+from scipy.stats import pearsonr, norm
+import pandas as pd
 
-def group_fix(partial_result, func, x, y_true, x_default, a=None, file_exist=False):
+def group_fix(partial_result, func, x, y_true, x_default, rand, a=None, file_exist=False):
     """
     Function for compare results between conditioned and unconditioned QoI.
     Fix parameters from the least influential group 
@@ -15,28 +16,32 @@ def group_fix(partial_result, func, x, y_true, x_default, a=None, file_exist=Fal
         D is the number of parameters
     y_true: list of func results with all x varying (the raw sampling matrix of x)
     x_default: scalar or listdefault values of x
+    rand: np.ndarray of resample index in bootstrap, shape of R * N where R is the number of resamples
     file_exist : Boolean for checking whether the partial results is from calculation
                 or reading from the existing file.
     a: coefficients used in func
     
     Return:
     =======
-    compare_mean : dict, changes in mean of the func results due to fixing parameters
-    compare_mae :dict, changes in absolute mean error of the func results due to fixing parameters
-    compare_var : dict, changes in variance of the func results due to fixing parameters
-    pearsons : dict, changes in pearson correlation coefficients 
+    mae : dict, changes in absolute mean error of the func results due to fixing parameters
+    var : dict, changes in variance of the func results due to fixing parameters
+    ppmc : dict, changes in pearson correlation coefficients 
                 of the func results due to fixing parameters
     """
     num_group = len(partial_result) - 1
     # store results from fixing parameters in dict
-    compare_mean = {i: None for i in range(num_group)}
-    compare_var = {i: None for i in range(num_group)}
-    compare_mae = {i: None for i in range(num_group)}
-    pearsons = {i: None for i in range(num_group)}
-    # sample_fix = {i: None for i in range(num_group)}
+    mae, var, ppmc = {}, {}, {}
+    mae_up, var_up, prsn_up = {}, {}, {}
+    mae_low, var_low, prsn_low = {}, {}, {}
+    # mae = {i: None for i in range(num_group)}
+    # ppmc = {i: None for i in range(num_group)}
+    # var_up = {i: None for i in range(num_group)}
+    # ppmc_up = {i: None for i in range(num_group)}
+    # mae_up = {i: None for i in range(num_group)}
+    # var_low = {i: None for i in range(num_group)}
+    # ppmc_low = {i: None for i in range(num_group)}
+    # mae_low = {i: None for i in range(num_group)}
     ind_fix = []
-    y_true_ave = np.mean(y_true)
-    y_true_var = np.var(y_true)
     for i in range(num_group, -1, -1):
         if file_exist:
             try:
@@ -48,26 +53,36 @@ def group_fix(partial_result, func, x, y_true, x_default, a=None, file_exist=Fal
                 ind_fix.extend(partial_result[i])
             except NameError:
                 ind_fix = partial_result[i]
-        sample_copy = np.copy(x) # for pce, x is sampled by dist.sample
+        x[:, ind_fix] = [x_default]
+        results_fix = func(x, a)
 
-        sample_copy[:, ind_fix] = [x_default]
-        if  a is None:
-            results_fix = func(*sample_copy.T)
-        else:
-            results_fix = func(sample_copy, a)
-            
         # compare results with insignificant parameters fixed
-        # calculate the average and expected error
-        compare_mean[i] = (np.square(results_fix - y_true)).mean(axis=0) / y_true_var
-        compare_var[i] = np.var(results_fix) / y_true_var
-        pearsons[i] = pearsonr(results_fix, y_true)
-#         sample_fix[i] = results_fix
-        # compare_mae[i] = np.abs((results_fix - y_true)).mean(axis=0) / y_true_ave
-        compare_mae[i] = (np.abs(results_fix - y_true) / y_true_ave).mean(axis=0)
-        # pearsons[i] = 1 - (((results_fix - y_true) **2).sum(axis=0) / y_true_var / len(y_true))
-    return compare_mean, compare_mae, compare_var, pearsons
+        Nresample = rand.shape[0]
+        var_bt,  ppmc_bt,  mae_bt = np.zeros(Nresample), \
+                                    np.zeros(Nresample), np.zeros(Nresample)
+        for ii in range(Nresample):            
+            I = rand[ii]
+            y_true_resample = y_true[I]
+            results_fix_resample = results_fix[I]
+            y_true_ave = y_true_resample.mean()
+            y_true_var = y_true_resample.var()
+            var_bt[ii] = results_fix_resample.var() / y_true_var
+            ppmc_bt[ii] = pearsonr(results_fix_resample, y_true_resample)[0]
+            mae_bt[ii] = np.abs(results_fix_resample - y_true_resample).mean(axis=0) / y_true_ave
+        
+        var[i] = var_bt.mean()
+        ppmc[i] = ppmc_bt.mean()
+        mae[i] = mae_bt.mean()
+        var_low[i], var_up[i] = np.quantile(var_bt, [0.025, 0.975])
+        ppmc_low[i], ppmc_up[i] = np.quantile(ppmc_bt, [0.025, 0.975])
+        mae_low[i], mae_up[i] = np.quantile(mae_bt, [0.025, 0.975])
+        dict_return = {'mae': mae, 'var': var, 'ppmc': ppmc,
+                        'mae_low': mae_low, 'var_low': var_low, 'ppmc_low': ppmc_low,
+                        'mae_up': mae_up, 'var_up': var_up, 'ppmc_up': ppmc_up}
+    
+    return dict_return
 
-def to_df(partial_order, fix_dict, y_true):
+def to_df(partial_order, fix_dict):
     """
     Help function to convert difference between 
     conditioned and unconditioned into dataframe.
@@ -76,7 +91,6 @@ def to_df(partial_order, fix_dict, y_true):
     partial_order : dict, partial ranking of parameters
     fix_dict : dict, difference between conditioned and unconditional model results.
                 (each dict result returned from group_fix / pce_group_fix)
-    y_true : numpy.ndarray, unconditional model results
 
     Returns:
     ========
