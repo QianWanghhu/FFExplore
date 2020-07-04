b09c6d1dd96ec4cc3dacf4d98d7e8bb8813bee83
Qian Wang <qian@hhu.edu.cn>
2020-07-04 16:26:10 +1000

    Add funcs for skipping runs
    Add functions to store results and skip runs of which the results have been calculated.

--- apply/utils/group_fix.py
+++ apply/utils/group_fix.py
@@ -2,7 +2,8 @@ import numpy as np
 from scipy.stats import pearsonr, norm
 import pandas as pd
 
-def group_fix(partial_result, func, x, y_true, x_default, rand, a=None, file_exist=False):
+def group_fix(partial_result, func, x, y_true, x_default, 
+            rand, pool_results, a=None, file_exist=False):
     """
     Function for compare results between conditioned and unconditioned QoI.
     Fix parameters from the least influential group 
@@ -31,17 +32,10 @@ def group_fix(partial_result, func, x, y_true, x_default, rand, a=None, file_exi
     num_group = len(partial_result) - 1
     # store results from fixing parameters in dict
     mae, var, ppmc = {}, {}, {}
-    mae_up, var_up, prsn_up = {}, {}, {}
-    mae_low, var_low, prsn_low = {}, {}, {}
-    # mae = {i: None for i in range(num_group)}
-    # ppmc = {i: None for i in range(num_group)}
-    # var_up = {i: None for i in range(num_group)}
-    # ppmc_up = {i: None for i in range(num_group)}
-    # mae_up = {i: None for i in range(num_group)}
-    # var_low = {i: None for i in range(num_group)}
-    # ppmc_low = {i: None for i in range(num_group)}
-    # mae_low = {i: None for i in range(num_group)}
+    mae_up, var_up, ppmc_low = {}, {}, {}
+    mae_low, var_low, ppmc_up = {}, {}, {}
     ind_fix = []
+    
     for i in range(num_group, -1, -1):
         if file_exist:
             try:
@@ -53,64 +47,92 @@ def group_fix(partial_result, func, x, y_true, x_default, rand, a=None, file_exi
                 ind_fix.extend(partial_result[i])
             except NameError:
                 ind_fix = partial_result[i]
-        x[:, ind_fix] = [x_default]
-        results_fix = func(x, a)
+        ind_fix.sort()        
+        # check whether results existing        
+        skip_calcul = results_exist(ind_fix, pool_results)
 
-        # compare results with insignificant parameters fixed
-        Nresample = rand.shape[0]
-        var_bt,  ppmc_bt,  mae_bt = np.zeros(Nresample), \
-                                    np.zeros(Nresample), np.zeros(Nresample)
-        for ii in range(Nresample):            
-            I = rand[ii]
-            y_true_resample = y_true[I]
-            results_fix_resample = results_fix[I]
-            y_true_ave = y_true_resample.mean()
-            y_true_var = y_true_resample.var()
-            var_bt[ii] = results_fix_resample.var() / y_true_var
-            ppmc_bt[ii] = pearsonr(results_fix_resample, y_true_resample)[0]
-            mae_bt[ii] = np.abs(results_fix_resample - y_true_resample).mean(axis=0) / y_true_ave
-        
-        var[i] = var_bt.mean()
-        ppmc[i] = ppmc_bt.mean()
-        mae[i] = mae_bt.mean()
-        var_low[i], var_up[i] = np.quantile(var_bt, [0.025, 0.975])
-        ppmc_low[i], ppmc_up[i] = np.quantile(ppmc_bt, [0.025, 0.975])
-        mae_low[i], mae_up[i] = np.quantile(mae_bt, [0.025, 0.975])
-        dict_return = {'mae': mae, 'var': var, 'ppmc': ppmc,
-                        'mae_low': mae_low, 'var_low': var_low, 'ppmc_low': ppmc_low,
-                        'mae_up': mae_up, 'var_up': var_up, 'ppmc_up': ppmc_up}
-    
-    return dict_return
+        if skip_calcul == False:
+            x_copy = np.copy(x)
+            x_copy[:, ind_fix] = [x_default]
+            results_fix = func(x_copy, a)
+            # compare results with insignificant parameters fixed
+            Nresample = rand.shape[0]
+            var_bt,  ppmc_bt,  mae_bt = np.zeros(Nresample), np.zeros(Nresample), np.zeros(Nresample)
 
-def to_df(partial_order, fix_dict):
+            for ii in range(Nresample):            
+                I = rand[ii]
+                y_true_resample = y_true[I]
+                results_fix_resample = results_fix[I]
+                y_true_ave = y_true_resample.mean()
+                y_true_var = y_true_resample.var()
+                var_bt[ii] = results_fix_resample.var() / y_true_var
+                ppmc_bt[ii] = pearsonr(results_fix_resample, y_true_resample)[0]
+                mae_bt[ii] = np.abs(results_fix_resample - y_true_resample).mean(axis=0) / y_true_ave
+            # End for()
+            
+            mae[i], var[i], ppmc[i] = mae_bt.mean(), var_bt.mean(), ppmc_bt.mean()
+            var_low[i], var_up[i] = np.quantile(var_bt, [0.025, 0.975])
+            ppmc_low[i], ppmc_up[i] = np.quantile(ppmc_bt, [0.025, 0.975])
+            mae_low[i], mae_up[i] = np.quantile(mae_bt, [0.025, 0.975])
+
+            # update pool_results
+            measure_list = [
+                            mae[i], var[i], ppmc[i], mae_low[i], mae_up[i], 
+                            var_low[i], var_up[i],ppmc_low[i], ppmc_up[i],
+                            ]
+
+            pool_results = pool_update(ind_fix, measure_list, pool_results)
+        else:
+            # map index to calculated values
+            [mae[i], var[i], ppmc[i], mae_low[i], mae_up[i],
+            var_low[i], var_up[i], ppmc_low[i], ppmc_up[i]] = skip_calcul
+
+    dict_return = {'mae': mae, 'var': var, 'ppmc': ppmc,
+                    'mae_low': mae_low, 'var_low': var_low, 'ppmc_low': ppmc_low,
+                    'mae_up': mae_up, 'var_up': var_up, 'ppmc_up': ppmc_up}
+    # End for()
+
+    return dict_return, pool_results
+
+def results_exist(parms_fixed, pool_results):
     """
-    Help function to convert difference between 
-    conditioned and unconditioned into dataframe.
-    Parameters:
+    Help function to check whether results existed already.
+    Parameters
     ===========
-    partial_order : dict, partial ranking of parameters
-    fix_dict : dict, difference between conditioned and unconditional model results.
-                (each dict result returned from group_fix / pce_group_fix)
+    parms_fixed : list, index of parameters that are to fix
+    pool_results : dict, contains both index of parameters fixed and the corresponding results
 
-    Returns:
-    ========
-    fix_df : df, formatted fix_dict
+    Return
+    =======
+    skip_cal
+    """ 
+    if pool_results == {}:
+        skip_cal = False
+    elif parms_fixed in pool_results['parms']:
+        index_measure = pool_results['parms'].index(parms_fixed) 
+        skip_cal = pool_results[f'measures_{index_measure}']
+    else:
+        skip_cal = False
+    return skip_cal
+    
+def pool_update(parms_fixed, measure_list, pool_results):
     """
-    keys_list = list(partial_order.keys())
+    Function used to update the pool_results.
+    Parameters
+    ===========
+    parms_fixed : list, index of parameters that are to fix
+    measure_list : list, measures newly calculated for parameters in parms_fixed
+    pool_results : dict, contains both index of parameters fixed and the corresponding results
 
-    fix_df = {key:None for key in keys_list}
+    Return:
+    pool_results
+    """
 
-    for key in keys_list:
-        len_each_group = []
-        len_each_group = [len(value) for k, value in partial_order[key].items()]
-        fix_temp = []
+    try:
+        pool_results['parms'].append(parms_fixed[:])
+    except KeyError:
+        pool_results['parms'] = [parms_fixed[:]]      
+    index_measure = pool_results['parms'].index(parms_fixed)
+    pool_results[f'measures_{index_measure}'] = measure_list
 
-        for g, v in fix_dict[key].items():
-            if isinstance(v, tuple):
-                fix_temp.extend([v[0]])
-            else:
-                fix_temp.extend([v])
-        fix_df[key]  = np.repeat(fix_temp, len_each_group)
-
-    fix_df = pd.DataFrame.from_dict(fix_df)
-    return fix_df
+    return pool_results
