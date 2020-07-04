52eedc8da77afe55d6c1ec0ea6952b10f91af1f1
Qian Wang <qian@hhu.edu.cn>
2020-07-04 16:27:43 +1000

    Update application with new functions

--- apply/apply_Morris.py
+++ apply/apply_Morris.py
@@ -11,47 +11,56 @@ from SALib.test_functions.Sobol_G import evaluate, total_sensitivity_index as to
 
 # import settings for Sobol G-function and returns necessary elements
 from utils.Sobol_G_setting import set_sobol_g_func
-from utils.group_fix import group_fix, to_df
+from utils.group_fix import group_fix
+from utils.partial_sort import to_df, partial_rank
 
 a, x, x_bounds, x_names, len_params, problem = set_sobol_g_func()
 f_dir = '../../../Research/G_func_ff/output/morris/revision/'
 cache_file = '{}{}'.format(f_dir, 'morris_test.json')
 
+# calculate results with fixed parameters
+x_all = sample_latin.sample(problem, 1000, seed=101)
+y_true = evaluate(x_all, a)
+y_true_ave = np.average(y_true)
+rand = np.random.randint(0, y_true.shape[0], size=(1000, y_true.shape[0]))
+x_default = 0.25
+error_dict = {}
+pool_res = {}
+
 if not os.path.exists(cache_file):
     # Loop of Morris
     file_exist = False
     partial_order = {}
-    mu_st, sigma_dt = {}
-    rank_low_dt, rank_up_dt,  = {}
-    for i in range(20, 100, 10):
+    mu_st, sigma_dt = {}, {}
+    rank_low_dt, rank_up_dt = {}, {}
+    n_start, n_end, n_step = 20, 120, 10
+    x_large_size = sample_morris.sample(problem, n_end, num_levels=4)
+    for i in range(n_start, n_end, n_step):
         # partial ordering
-        x_morris= sample_morris.sample(problem, i, num_levels=4)
-        y_morris = evaluate(x_morris, a)
+        x_morris= x_large_size[:i * (len_params + 1)]
+        if i == n_start:
+            y_morris = evaluate(x_morris, a)
+        else:
+            y_eval = evaluate(x_morris[-(len_params + 1) * n_step:], a)
+            y_morris =  np.append(y_morris, y_eval)
         sa_dict= analyze_morris.analyze(problem, x_morris, y_morris, num_resamples=1000, conf_level=0.95, seed=123)
         mu_star_rank_dict = sa_dict['mu_star'].argsort().argsort()
         # use toposort to find parameter sa block
         conf_low = sa_dict['mu_star_rank_conf'][0]
         conf_up = sa_dict['mu_star_rank_conf'][1]
-        rank_conf = {j:None for j in range(len_params)}
-        for j in range(len_params):
-            rank_conf[j] = [conf_low[j], conf_up[j]]  
-        abs_sort= {j:None for j in range(len_params)}
 
-        for m in range(len_params):
-            list_temp = np.where(conf_low >= conf_up[m])
-            set_temp = set()
-            if len(list_temp) > 0:
-                for ele in list_temp[0]:
-                    set_temp.add(ele)
-            abs_sort[m] = set_temp
-        order_temp = list(toposort(abs_sort))
+        abs_sort = partial_rank(len_params, conf_low, conf_up)
+        rank_list = list(toposort(abs_sort))
+        key = 'result_'+str(i)     
+        partial_order[key] = {j: list(rank_list[j]) for j in range(len(rank_list))}
+        #save results returned from Morris if needed
+        mu_st[key] = sa_dict['mu_star']
+        rank_low_dt[key] = conf_low
+        rank_up_dt[key] =  conf_up
+        sigma_dt[key] = sa_dict['sigma']
 
-        partial_order['result_'+str(i)] = {j: list(order_temp[j]) for j in range(len(order_temp))}
-#         #save results of mu_star
-        mu_st['result_'+str(i)] = sa_dict['mu_star']
-        rank_low_dt['result_'+str(i)] = conf_low
-        rank_up_dt['result_'+str(i)] =  conf_up
-        sigma_dt['result_'+str(i)] = sa_dict['sigma']
+        error_dict[key], pool_res = group_fix(partial_order[key], evaluate, 
+                        x_all, y_true, x_default, rand, pool_res, a, file_exist)
 
     with open(cache_file, 'w') as fp:
         json.dump(partial_order, fp, indent=2)
@@ -59,33 +68,17 @@ else:
     file_exist = True
     with open(cache_file, 'r') as fp:
         partial_order = json.load(fp)
-# End if-else
+    for key, value in partial_order.items():
+        error_dict[key], pool_res = group_fix(value, evaluate, x_all, y_true, 
+                                        x_default, rand, pool_res, a, file_exist)                                       
+# End If-Else
 
-# calculate results with fixed parameters
-x_all = sample_latin.sample(problem, 10000, seed=101)
-y_true = evaluate(x_all, a)
-y_true_ave = np.average(y_true)
-keys_list = list(partial_order.keys())[0:11]
-partial_order = {k:v for k, v in partial_order.items() if k in keys_list}
-rand = np.random.randint(0, y_true.shape[0], size=(1000, y_true.shape[0]))
-# run more for default values evaluation
-# put all default values to test into a list
+
 # defaults_list = np.append([0, 0.1, 0.2, 0.4, 0.5], np.round(np.linspace(0.21, 0.3, 10), 2))
 # defaults_list.sort()
-defaults_list = [0.25]
-for x_default in defaults_list: 
-    mae, var, ppmc = {}, {}
-    var_low, var_up = {}, {}
-    ppmc_low, ppmc_up = {}, {}
-    mae_low, mae_up = {}, {}
-    for key in keys_list:
-        mae[key], var[key], ppmc[key], mae_low[key], \
-        var_low[key], ppmc_low[key], mae_up[key], var_up[key], ppmc_up[key] = \
-        group_fix(partial_order[key], evaluate, x_all, y_true, x_default, rand, a, file_exist)
-
-    # convert the result into dataframe
-    dict_lists = [mae, var, ppmc, mae_low, var_low, ppmc_low, mae_up, var_up, ppmc_up]
-    # f_names = ['mae', 'var', 'pearson', 'mae_low', 'var_low', 'pearson_low', 'mae_up', 'var_up', 'pearson_up']
-    # for ele in range(len(dict_lists)):
-    #     df = to_df(partial_order, dict_lists[ele])
-    #     df.to_csv(f'{f_dir}test/seed123/{f_names[ele]}.csv')
+# convert the result into dataframe
+# dict_lists = [mae, var, ppmc, mae_low, var_low, ppmc_low, mae_up, var_up, ppmc_up]
+# f_names = ['mae', 'var', 'pearson', 'mae_low', 'var_low', 'pearson_low', 'mae_up', 'var_up', 'pearson_up']
+# for ele in range(len(dict_lists)):
+#     df = to_df(partial_order, dict_lists[ele])
+#     df.to_csv(f'{f_dir}test/seed123/{f_names[ele]}.csv')
--- apply/apply_Sobol.py
+++ apply/apply_Sobol.py
@@ -13,14 +13,22 @@ from SALib.test_functions.Sobol_G import evaluate, \
     partial_first_order_variance as pfoa, \
     total_variance
 # import settings for Sobol G-function and returns necessary elements
-%load_ext autoreload
-%autoreload 2
+# import settings for Sobol G-function and returns necessary elements
 from utils.Sobol_G_setting import set_sobol_g_func
-from utils.group_fix import group_fix, to_df
-a, x, x_bounds, x_names, len_params, problem = set_sobol_g_func()
+from utils.group_fix import group_fix
+from utils.partial_sort import to_df, partial_rank
 
+a, x, x_bounds, x_names, len_params, problem = set_sobol_g_func()
 f_dir = '../../../Research/G_func_ff/output/sobol/revision/'
-cache_file = '{}{}'.format(f_dir, 'sobol.json')
+cache_file = '{}{}'.format(f_dir, 'sobol_test.json')
+# calculate results with fixed parameters
+x_all = sample_latin.sample(problem, 1000, seed=101)
+y_true = evaluate(x_all, a)
+y_true_ave = np.average(y_true)
+x_default = 0.25
+rand = np.random.randint(0, y_true.shape[0], size=(1000, y_true.shape[0]))
+error_dict = {}
+pool_res = {}
 
 if not os.path.exists(cache_file):
     # Loop of Morris
@@ -28,69 +36,41 @@ if not os.path.exists(cache_file):
     partial_order = {}
     sa_total = {}
     sa_main = {}
-    for i in range(600, 1200, 200):
+    n_start, n_end, n_step = 200, 1200, 200
+    x_large_size = sample_saltelli.sample(problem, n_end, calc_second_order=False)
+    for i in range(n_start, n_end, n_step):
         # partial ordering
-        x_sobol = sample_saltelli.sample(problem, i, calc_second_order=True)
-        print(x_sobol.shape)
-        y_sobol = g_func(x_sobol, a)
+        x_sobol = x_large_size[:i * (len_params + 2)]
+        if i == n_start:
+            y_sobol = evaluate(x_sobol, a)
+        else:
+            y_eval = evaluate(x_sobol[-(len_params + 2) * n_step:], a)
+            y_sobol =  np.append(y_sobol, y_eval)
+
+        y_sobol = evaluate(x_sobol, a)
         sa_sobol = analyze_sobol.analyze(problem, 
-                    y_sobol, calc_second_order=True, num_resamples=1000, conf_level=0.95)
-        # mu_star_rank_dict = sa_dict['mu_star'].argsort().argsort()
+                    y_sobol, calc_second_order=False, num_resamples=1000, conf_level=0.95)
         # use toposort find parameter sa block
         conf_low = sa_sobol['total_rank_ci'][0]
         conf_up = sa_sobol['total_rank_ci'][1]
 
-        rank_conf = {j:None for j in range(len_params)}
-        for j in range(len_params):
-            rank_conf[j] = [conf_low[j], conf_up[j]]  
-        abs_sort= {j:None for j in range(len_params)}
+        abs_sort = partial_rank(len_params, conf_low, conf_up)
+        rank_list = list(toposort(abs_sort))
 
-        for m in range(len_params):
-            list_temp = np.where(conf_low >= conf_up[m])
-            set_temp = set()
-            if len(list_temp) > 0:
-                for ele in list_temp[0]:
-                    set_temp.add(ele)
-            abs_sort[m] = set_temp
-        order_temp = list(toposort(abs_sort))
-
-        partial_order['result_'+str(i)] = {j: list(order_temp[j]) for j in range(len(order_temp))}
+        key = 'result_'+str(i)
+        partial_order[key] = {j: list(rank_list[j]) for j in range(len(rank_list))}
 #         #save results of sobol indices
-        sa_total['result_'+str(i)] = sa_sobol['ST']         
-        sa_main['result_'+str(i)] = sa_sobol['S1']
+        sa_total[key] = sa_sobol['ST']         
+        sa_main[key] = sa_sobol['S1']
 
-
+        error_dict[key], pool_res = group_fix(partial_order[key], evaluate, 
+                        x_all, y_true, x_default, rand, pool_res, a, file_exist)
     with open(cache_file, 'w') as fp:
         json.dump(partial_order, fp, indent=2)
 else:
     file_exist = True
     with open(cache_file, 'r') as fp:
         partial_order = json.load(fp)
-
-
-
-# calculate results with fixed parameters
-x_all = sample_latin.sample(problem, 10000, seed=101)
-y_true = evaluate(x_all, a)
-y_true_ave = np.average(y_true)
-keys_list = list(partial_order.keys())[:12]
-partial_order = {k:v for k, v in partial_order.items() if k in keys_list}
-rand = np.random.randint(0, y_true.shape[0], size=(1000, y_true.shape[0]))
-defaults_list = [0.25]
-for x_default in defaults_list: 
-    mae_conf, var_conf = {key: None for key in keys_list}, {key: None for key in keys_list}
-    var_fix = {key: None for key in keys_list}
-    mae_fix = {key: None for key in keys_list}
-    prsn_fix = {key: None for key in keys_list}
-    pearsonr_conf = {key: None for key in keys_list}
-    for key in keys_list:
-        mae_fix[key], var_fix[key], prsn_fix[key], mae_conf[key], var_conf[key], pearsonr_conf[key] \
-        = group_fix(partial_order[key], evaluate, x_all, y_true, x_default, rand, a, file_exist)
-
-    # convert the result into dataframe
-    dict_lists = [mae_fix, var_fix, prsn_fix, mae_conf, var_conf, pearsonr_conf]
-    f_names = ['mae', 'var', 'pearsonr', 'mae_conf', 'var_conf', 'pearsonr_conf']
-    for ele in range(len(dict_lists)):
-        df = to_df(partial_order, dict_lists[ele])
-        df.to_csv(f'{f_dir}{f_names[ele]}.csv')
-# End for()        
+    for key, value in partial_order.items():
+        error_dict[key], pool_res = group_fix(value, evaluate, x_all, y_true, 
+                                        x_default, rand, pool_res, a, file_exist)
