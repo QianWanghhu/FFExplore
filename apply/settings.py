15d5ea702ac404011bd3af9516da595f9a581d8f
ConnectedSystems <takuyai@gmail.com>
2020-07-05 12:47:19 +1000

    Added blank lines to satisfy PEP8

--- apply/settings.py
+++ apply/settings.py
@@ -1,2 +1,2 @@
 MORRIS_DATA_DIR = '../../../Research/G_func_ff/output/morris/revision/'
-SOBOL_DATA_DIR = '../../../Research/G_func_ff/output/sobol/revision/' 
+SOBOL_DATA_DIR = '../../../Research/G_func_ff/output/sobol/revision/'
