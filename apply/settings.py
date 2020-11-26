1b4bd945dcabfc1e06371420306a850060c75b4c
Qian Wang <qian@hhu.edu.cn>
2020-11-27 01:06:56 +0800

    Add metric output names

--- apply/settings.py
+++ apply/settings.py
@@ -1,3 +1,5 @@
 MORRIS_DATA_DIR = 'output/morris/'
 SOBOL_DATA_DIR = 'output/sobol/'
 FIGURE_DIR = 'output/figure/'
+METRIC_NAME = ['mae', 'var', 'ppmc', 'mae_upper', 'var_upper', 
+            'ppmc_upper', 'mae_lower', 'var_lower', 'ppmc_lower'] 
