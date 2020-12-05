cd7d5b957414b968f1dcf33ade50521fbebb3813
Qian Wang <qian@hhu.edu.cn>
2020-12-05 22:13:57 +0800

    Update to use new group_fix

--- apply/apply_Morris.py
+++ apply/apply_Morris.py
@@ -14,84 +14,113 @@ from SALib.test_functions.Sobol_G import evaluate, total_sensitivity_index as to
 
 # import settings for Sobol G-function and returns necessary elements
 from utils.Sobol_G_setting import set_sobol_g_func, add_dummy
-from utils.group_fix import group_fix
 from utils.partial_sort import to_df, partial_rank
+from utils.group_fix import group_fix, index_fix, results_exist
+from utils.group_fix import evaluate_wrap
 
 from settings import MORRIS_DATA_DIR
 
 a, x, x_bounds, x_names, len_params, problem = set_sobol_g_func()
-cache_file = f'{MORRIS_DATA_DIR}morris_{8}.json'
+cache_file = f'../output/morris/morris_test.json'
+file_exists = os.path.exists(cache_file)
+if not file_exists:
+    # Loop of Morris
+    partial_order = {}
+    mu_st, sigma_dt = {}, {}
+    rank_lower_dt, rank_upper_dt = {}, {}
+    n_start, n_end, n_step = 10, 130, 10
+    x_large_size = sample_morris.sample(problem, n_end, num_levels=8, seed=123)
+    for i in range(n_start, n_end, n_step):
+        # partial ordering
+        x_morris = x_large_size[:i * (len_params + 1)]
+        if i == n_start:
+            y_morris = evaluate(x_morris, a)
+        else:
+            y_eval = evaluate(x_morris[-(len_params + 1) * n_step:], a)
+            y_morris =  np.append(y_morris, y_eval)
+        sa_dict = analyze_morris.analyze(problem, x_morris, y_morris, num_resamples=1000, conf_level=0.95, seed=123)
+        mu_star_rank_dict = sa_dict['mu_star'].argsort().argsort()
 
-# calculate results with fixed parameters
-x_all = sample_latin.sample(problem, 10000)#, seed=101
-y_true = evaluate(x_all, a)
-y_true_ave = np.average(y_true)
-rand = np.random.randint(0, y_true.shape[0], size=(1000, y_true.shape[0]))
-defaults_list = np.append([0, 0.1, 0.2, 0.4, 0.5], np.round(np.linspace(0.21, 0.3, 10), 2))
-# defaults_list = [0.25]
+        # use toposort to find parameter sa block
+        conf_lower = sa_dict['mu_star_rank_conf'][0]
+        conf_upper = sa_dict['mu_star_rank_conf'][1]
+
+        abs_sort = partial_rank(len_params, conf_lower, conf_upper)
+        
+        rank_list = list(toposort(abs_sort))
+        key = 'result_'+str(i)     
+        partial_order[key] = {j: list(rank_list[j]) for j in range(len(rank_list))}
+
+        #save results returned from Morris if needed
+        mu_st[key] = sa_dict['mu_star']
+        rank_lower_dt[key] = conf_lower
+        rank_upper_dt[key] = conf_upper
+        sigma_dt[key] = sa_dict['sigma']
+
+    with open(cache_file, 'w') as fp:
+        json.dump(partial_order, fp, indent=2)
+else:
+    with open(cache_file, 'r') as fp:
+        partial_order = json.load(fp)
+
+        
+# End
+# defaults_list = np.append([0, 0.1, 0.2, 0.4, 0.5], np.round(np.linspace(0.21, 0.3, 10), 2))
+defaults_list = [0.25]
 defaults_list.sort()
 
 for x_default in defaults_list:
-    error_dict = {}
-    pool_res = {}
-    out_path = f'{MORRIS_DATA_DIR}{x_default}'
-    if not os.path.exists(out_path):
-        os.makedirs(out_path)
+    out_path = f'../output/morris/latin_adaptive/{x_default}'
+    if not os.path.exists(out_path): os.makedirs(out_path)
 
-    file_exists = os.path.exists(cache_file)
-    if not file_exists:
-        # Loop of Morris
-        partial_order = {}
-        mu_st, sigma_dt = {}, {}
-        rank_lower_dt, rank_upper_dt = {}, {}
-        n_start, n_end, n_step = 10, 150, 10
-        x_large_size = sample_morris.sample(problem, n_end, num_levels=8, seed=1010) #
-        for i in range(n_start, n_end, n_step):
-            # partial ordering
-            x_morris = x_large_size[:i * (len_params + 1)]
-            if i == n_start:
-                y_morris = evaluate(x_morris, a)
-            else:
-                y_eval = evaluate(x_morris[-(len_params + 1) * n_step:], a)
-                y_morris =  np.append(y_morris, y_eval)
-            sa_dict = analyze_morris.analyze(problem, x_morris, y_morris, num_resamples=1000, conf_level=0.95, seed=123)
-            mu_star_rank_dict = sa_dict['mu_star'].argsort().argsort()
+    error_dict = {}; pool_res = {}
+    y_fix = np.array([])
+    # get input samples
+    file_sample = f'../output/morris/metric_samples.csv'
+    if os.path.exists(file_sample):
+        y_true_exist = True
+        samples = pd.read_csv(file_sample, index_col = 'Unnamed: 0').values
+        x_all = samples[:, 0:-1]
+        y_true = samples[:, -1]  
+        x_subset = x_all[:10]
+        y_subset = y_true[:10]
 
-            # use toposort to find parameter sa block
-            conf_lower = sa_dict['mu_star_rank_conf'][0]
-            conf_upper = sa_dict['mu_star_rank_conf'][1]
-
-            abs_sort = partial_rank(len_params, conf_lower, conf_upper)
+    for key, value in partial_order.items():
+        num_group = len(value) - 1
+        mae = {i: None for i in range(num_group)}
+        var, ppmc = dict(mae), dict(mae)
+        mae_upper, var_upper, ppmc_upper = dict(mae), dict(mae), dict(mae)
+        mae_lower, var_lower, ppmc_lower = dict(mae), dict(mae), dict(mae)
+        ind_fix = []
+        # loop to fix parameters and calculate the error metrics
+        for i in range(num_group, -1, -1):
+            ind_fix = index_fix(value, i, file_exists, ind_fix)    
             
-            rank_list = list(toposort(abs_sort))
-            key = 'result_'+str(i)     
-            partial_order[key] = {j: list(rank_list[j]) for j in range(len(rank_list))}
+            skip_calcul = results_exist(ind_fix, pool_res)
+            if skip_calcul == False:
+                x_copy = np.copy(x_subset)
+                x_copy[:, ind_fix] = [x_default]
+                y_fix = evaluate_wrap(evaluate, x_copy, a)
+                y_true_ave = np.average(y_subset)
+                rand = np.random.randint(0, y_subset.shape[0], size=(1000, y_subset.shape[0]))
+                error_temp, pool_res, _ = group_fix(ind_fix, y_subset, y_fix, rand, pool_res, file_exists)
+            
+            else:
+                # map index to calculated values
+                error_temp = skip_calcul
 
-            #save results returned from Morris if needed
-            mu_st[key] = sa_dict['mu_star']
-            rank_lower_dt[key] = conf_lower
-            rank_upper_dt[key] = conf_upper
-            sigma_dt[key] = sa_dict['sigma']
+            [mae[i], var[i], ppmc[i], mae_lower[i], var_lower[i], ppmc_lower[i], 
+            mae_upper[i],var_upper[i], ppmc_upper[i]] = error_temp
 
-            # error_dict[key], pool_res = group_fix(partial_order[key], evaluate, 
-            #                                     x_all, y_true, x_default, rand, 
-            #                                     pool_res, a, file_exists)
+        error_dict[key] = {'mae': mae, 'var': var, 'ppmc': ppmc,
+                    'mae_lower': mae_lower, 'var_lower': var_lower, 'ppmc_lower': ppmc_lower,
+                    'mae_upper': mae_upper, 'var_upper': var_upper, 'ppmc_upper': ppmc_upper}
+        # # End for
 
-        with open(cache_file, 'w') as fp:
-            json.dump(partial_order, fp, indent=2)
-    else:
-        with open(cache_file, 'r') as fp:
-            partial_order = json.load(fp)
-
-        # for key, value in partial_order.items():
-        #     error_dict[key], pool_res = group_fix(value, evaluate, x_all, y_true, 
-        #                                         x_default, rand, pool_res, a, file_exists)
-# End
-
-    # # convert the result into dataframe
-    # key_outer = list(error_dict.keys())
-    # f_names = list(error_dict[key_outer[0]].keys())
-    # for ele in f_names:
-    #     dict_measure = {key: error_dict[key][ele] for key in key_outer}
-    #     df = to_df(partial_order, dict_measure)
-    #     df.to_csv(f'{MORRIS_DATA_DIR}{x_default}/{ele}.csv')
+    # convert the result into dataframe
+    key_outer = list(error_dict.keys())
+    f_names = list(error_dict[key_outer[0]].keys())
+    for ele in f_names:
+        dict_measure = {key: error_dict[key][ele] for key in key_outer}
+        df = to_df(partial_order, dict_measure)
+        df.to_csv(f'{out_path}/{ele}.csv')
--- apply/compare_two_sa.py
+++ apply/compare_two_sa.py
@@ -1,86 +1,38 @@
-"""Apply experiment with Sobol."""
+"""Apply experiment with Morris."""
 
 import pandas as pd
 import numpy as np
-from toposort import toposort, toposort_flatten
 import json
 import os
 
 #sensitivity analysis and partial sorting 
-from SALib.sample import saltelli as sample_saltelli
-from SALib.analyze import sobol as analyze_sobol
-from SALib.sample import latin as sample_latin
 from SALib.test_functions.Sobol_G import evaluate, total_sensitivity_index as total_sa
 
 # import settings for Sobol G-function and returns necessary elements
-from utils.Sobol_G_setting import set_sobol_g_func, add_dummy
-from utils.partial_sort import to_df, partial_rank
+from utils.partial_sort import to_df
+from utils.Sobol_G_setting import set_sobol_g_func
 from utils.group_fix import group_fix, index_fix, results_exist
 from utils.group_fix import evaluate_wrap
 
-from settings import SOBOL_DATA_DIR
-
+from settings import MORRIS_DATA_DIR, SOBOL_DATA_DIR
 a, x, x_bounds, x_names, len_params, problem = set_sobol_g_func()
-cache_file = f'../output/sobol/sobol_123.json'
+cache_file = [f'../output/morris/morris_8_123.json', f'../output/sobol/sobol_123.json' ]
+file_exists = True
+pool_res = {}; x_default = 0.25
 
-seed = 123; dummy = False
-file_exists = os.path.exists(cache_file)
-if not file_exists:
-    # Loop of Morris
-    partial_order = {}
-    sa_main, sa_main_conf = {}, {}
-    sa_total, sa_total_conf = {}, {}
-    n_start, n_end, n_step = 200, 1200, 200
-    x_large_size = sample_saltelli.sample(problem, n_end, calc_second_order=False, seed=seed)
-    for i in range(n_start, n_end, n_step):
-        # partial ordering
-        x_sobol = x_large_size[:i * (len_params + 2)]
-        if i == n_start:
-            y_sobol = evaluate(x_sobol, a)
-        else:
-            y_eval = evaluate(x_sobol[-(len_params + 2) * n_step:], a)
-            # y_eval = evaluate(x_morris[-(len_params + 1) * n_step:], a)
-            y_sobol =  np.append(y_sobol, y_eval)
-        sa_sobol = analyze_sobol.analyze(problem, y_sobol, 
-                        calc_second_order=False, num_resamples=1000, conf_level=0.95, dummy=dummy)
-
-        # use toposort to find parameter sa block
-        conf_lower = sa_sobol['total_rank_ci'][0]
-        conf_upper = sa_sobol['total_rank_ci'][1]
-        if dummy: 
-            len_dummy = len_params + 1
-            abs_sort = partial_rank(len_dummy, conf_lower, conf_upper)
-        else:
-            abs_sort = partial_rank(len_params, conf_lower, conf_upper)
-
-        rank_list = list(toposort(abs_sort))
-        key = 'result_'+str(i)     
-        partial_order[key] = {j: list(rank_list[j]) for j in range(len(rank_list))}
-
-        #save results returned from Morris if needed
-        sa_total[key] = sa_sobol['ST']
-        sa_total_conf[key] = sa_sobol['ST_conf']
-        sa_main[key] = sa_sobol['S1']
-        sa_main_conf[key] = sa_sobol['S1_conf']
-
-    with open(cache_file, 'w') as fp:
-        json.dump(partial_order, fp, indent=2)
-else:
-    with open(cache_file, 'r') as fp:
+for fn in cache_file:
+    # select which cache file to use
+    with open(fn, 'r') as fp:
         partial_order = json.load(fp)
 
-        
-# End
-# End
-# defaults_list = np.append([0, 0.1, 0.2, 0.4, 0.5], np.round(np.linspace(0.21, 0.3, 10), 2))
-defaults_list = [0.25]
-defaults_list.sort()
+    if 'morris' in fn:
+        out_path = f'../output/morris/latin_adaptive/{x_default}'
+    else:
+        key = list(partial_order.keys())[3]; partial_order = {key: partial_order[key]}
+        out_path = f'../output/sobol/{x_default}'
 
-for x_default in defaults_list:
-    out_path = f'../output/sobol/{x_default}'
     if not os.path.exists(out_path): os.makedirs(out_path)
-
-    error_dict = {}; pool_res = {}
+    error_dict = {} 
     y_fix = np.array([])
     # get input samples
     file_sample = f'../output/morris/metric_samples.csv'
@@ -130,6 +82,4 @@ for x_default in defaults_list:
     for ele in f_names:
         dict_measure = {key: error_dict[key][ele] for key in key_outer}
         df = to_df(partial_order, dict_measure)
-        fpath = f'../output/sobol/{x_default}'
-        if not os.path.exists(fpath): os.makedirs(fpath)
-        df.to_csv(f'{fpath}/{ele}.csv')
+        df.to_csv(f'{out_path}/{ele}.csv')
