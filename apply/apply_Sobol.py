87d3bc36c009b61505ca653d5895183950925b18
Qian Wang <qian@hhu.edu.cn>
2020-12-11 09:54:20 +0800

    Reuse Saltelli sampling for error metrics

--- apply/apply_Sobol.py
+++ apply/apply_Sobol.py
@@ -21,32 +21,31 @@ from utils.group_fix import evaluate_wrap
 from settings import SOBOL_DATA_DIR
 
 a, x, x_bounds, x_names, len_params, problem = set_sobol_g_func()
-cache_file = f'../output/sobol/sobol_main_123.json'
+cache_file = f'../output/sobol/sobol_test.json'
 
-seed = 123; dummy = False
+seed = np.random.randint(0, 1000); dummy = False
 file_exists = os.path.exists(cache_file)
 if not file_exists:
     # Loop of Morris
     partial_order = {}
     sa_main, sa_main_conf = {}, {}
     sa_total, sa_total_conf = {}, {}
-    n_start, n_end, n_step = 200, 6000, 200
-    x_large_size = sample_saltelli.sample(problem, n_end, calc_second_order=False, seed=seed)
-    for i in range(n_start, n_end, n_step):
+    nstart, nstop, nstep = 200, 801, 200
+    x_large_size = sample_saltelli.sample(problem, nstop, calc_second_order=False, seed=seed)
+    for i in range(nstart, nstop, nstep):
         # partial ordering
         x_sobol = x_large_size[:i * (len_params + 2)]
-        if i == n_start:
+        if i == nstart:
             y_sobol = evaluate(x_sobol, a)
         else:
-            y_eval = evaluate(x_sobol[-(len_params + 2) * n_step:], a)
-            # y_eval = evaluate(x_morris[-(len_params + 1) * n_step:], a)
+            y_eval = evaluate(x_sobol[-(len_params + 2) * nstep:], a)
+            # y_eval = evaluate(x_morris[-(len_params + 1) * nstep:], a)
             y_sobol =  np.append(y_sobol, y_eval)
         sa_sobol = analyze_sobol.analyze(problem, y_sobol, 
-                        calc_second_order=False, num_resamples=1000, conf_level=0.95, dummy=dummy)
-
+                        calc_second_order=False, num_resamples=1000, conf_level=0.95)
         # use toposort to find parameter sa block
-        conf_lower = sa_sobol['main_rank_ci'][0]
-        conf_upper = sa_sobol['main_rank_ci'][1]
+        conf_lower = sa_sobol['total_rank_ci'][0]
+        conf_upper = sa_sobol['total_rank_ci'][1]
         if dummy: 
             len_dummy = len_params + 1
             abs_sort = partial_rank(len_dummy, conf_lower, conf_upper)
@@ -68,3 +67,16 @@ if not file_exists:
 else:
     with open(cache_file, 'r') as fp:
         partial_order = json.load(fp)
+
+
+# Obtain A and B metrics
+from SALib.analyze.sobol import separate_output_values
+index_y = np.arange(0, x_sobol.shape[0])
+index_A, index_B, _, _ = separate_output_values(index_y, 
+    problem['num_vars'], nstop-1, False)
+index_AB = np.append(index_A, index_B)
+y = y_sobol[index_AB]
+x = x_sobol[index_AB, :]
+xy_df = pd.DataFrame(data = x, index = np.arange(0, x.shape[0]), columns = problem['names'])
+xy_df.loc[:, 'y'] = y
+xy_df.to_csv(f'../output/sobol/satelli_samples.csv')
