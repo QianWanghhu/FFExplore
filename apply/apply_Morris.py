8845e8d08074b194603b6ce1e479738a817ebe8e
QianW <qian@hhu.edu.cn>
2021-01-14 22:00:37 +0800

    Delete the combs of bootstrap and replicates.

--- apply/apply_Morris.py
+++ apply/apply_Morris.py
@@ -13,7 +13,8 @@ from utils.group_fix import evaluate_wrap
 
 from settings import METRIC_NAME, MORRIS_DATA_DIR
 from sa_rankings import morris_ranking
-from utils.sample_replicates import sample_repli
+from utils.sample_replicates import sample_repli, return_metric_samples
+from utils.sample_replicates import replicates_process, boot_process
 
 a, x, x_bounds, x_names, len_params, problem = set_sobol_g_func()
 
@@ -27,9 +28,10 @@ else:
     partial_order, x_morris = morris_ranking(cache_file)
     np.savetxt(outer_path + 'morris_sample.txt', x_morris)
 
-desti_folder = 'vertical'
-out_path = f'{outer_path}{desti_folder}/'
-if not os.path.exists(out_path): os.makedirs(out_path)
+desti_folder = ['bootstrap', 'vertical', 'horizontal']
+out_path  = [f'{outer_path}{f}/' for f in desti_folder]
+for f in out_path: 
+    if not os.path.exists(f): os.makedirs(f)
 
 # Define the factor set to fix and the sample size to use
 x_fix_set = [[i for i in range(4, 21)], [i for i in range(8, 21)], 
@@ -37,41 +39,47 @@ x_fix_set = [[i for i in range(4, 21)], [i for i in range(8, 21)],
 x_default = 0.25
 
 # get input samples
-metric_cache = f'{outer_path}{desti_folder}/metric_samples.txt'
 r = 10 # r is the number of repetitions
-if os.path.exists(metric_cache): 
-    samples = np.loadtxt(metric_cache)
-else:
-    samples = sample_repli(800, len_params, metric_cache, split_style = 'vertical', 
-        skip_numbers = 1000, num_replicates = r)
+split_style = 'vertical'; size = 800; skip_numbers = 1000; 
+metric_cache = f'{out_path[0]}metric_samples.txt'
+samples_vertical = return_metric_samples(metric_cache, size, len_params, split_style, skip_numbers, r)
+metric_cache = f'{out_path[1]}metric_samples.txt'
+samples_horizontal = return_metric_samples(metric_cache, size, len_params, 'horizontal', skip_numbers, r)
 
-nsubsets = int(samples.shape[0] / 10); nstart = 9; nboot=1
+# calculation with bootstrap
+boot = True
+samples_boot = np.vstack(np.hsplit(samples_vertical, r))
+nsubsets = int(samples_boot.shape[0] / 100); nstart = 0; nboot=1000
+loop_error_metrics(out_path[0], x_fix_set, x_default, nsubsets, 1, len_params, 
+    samples_boot, evaluate, boot, file_exists, a = a, nboot = nboot, save_file=True)
+
+# calculation with replicate sampling
 boot = False
+nsubsets = int(samples_vertical.shape[0] / 10); nstart = 0; nboot=1
+loop_error_metrics(out_path[1], x_fix_set, x_default, nsubsets, r, len_params, 
+    samples_vertical, evaluate, boot, file_exists, a = a, nboot = nboot, save_file=True)
 
-loop_error_metrics(out_path, x_fix_set, x_default, nsubsets, r, len_params, 
-    samples, evaluate, boot, file_exists, a = a, nboot = nboot, save_file=True)
+loop_error_metrics(out_path[2], x_fix_set, x_default, nsubsets, r, len_params, 
+    samples_horizontal, evaluate, boot, file_exists, a = a, nboot = nboot, save_file=True)
 
 # Calculate the standard error and mean
-out_path = f'{outer_path}{desti_folder}/'
-err_metrics = ['mae', 'var', 'ppmc']
-col_names = [i + '_mean' for i in err_metrics]
-for i in err_metrics: col_names.append(i + '_std')
-from utils.sample_replicates import replicates_process, boot_process
-if 'boot' in desti_folder:
-    boot_process(out_path, col_names, nsubsets, r, save_file=True)
-else:
-    replicates_process(out_path, col_names, nsubsets, r, save_file=True)
+for f in out_path[1:]:
+    err_metrics = ['mae', 'var', 'ppmc']
+    col_names = [i + '_mean' for i in err_metrics]
+    for i in err_metrics: col_names.append(i + '_std')
+    replicates_process(f, col_names, nsubsets, r, save_file=True)
 
+##TODO clean up the following
 # Default evaluation
 defaults_list = np.append([0, 0.1, 0.2, 0.4, 0.5], np.round(np.linspace(0.21, 0.3, 10), 2))
 defaults_list.sort()
-samples  = samples[0:100, :]
-nsubsets = int(samples.shape[0] / 10)
+samples_vertical  = samples_vertical[0:100, :]
+nsubsets = int(samples_vertical.shape[0] / 10)
 for x_default in defaults_list:
     out_path = f'{outer_path}{desti_folder}/{x_default}/'
     if not os.path.exists(out_path): os.makedirs(out_path)
     loop_error_metrics(out_path, x_fix_set[-2:], x_default, nsubsets, r, len_params, 
-        samples, evaluate, boot, file_exists, a = a, nboot = nboot, save_file=True, nstart=9)
+        samples_vertical, evaluate, boot, file_exists, a = a, nboot = nboot, save_file=True, nstart=9)
 
 num_defaults = len(defaults_list)
 num_fix = ['fix_6', 'fix_9']
