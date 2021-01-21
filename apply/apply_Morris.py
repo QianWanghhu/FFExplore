4d8a2a0dca6c8114bf5be475a8999ce4b2464994
QianW <qian@hhu.edu.cn>
2021-01-21 11:02:14 +0800

    1) Untrack output; 2) Adaptive replicates

--- apply/apply_Morris.py
+++ apply/apply_Morris.py
@@ -19,7 +19,7 @@ from utils.sample_replicates import replicates_process, boot_process
 a, x, x_bounds, x_names, len_params, problem = set_sobol_g_func()
 
 # sensitivity analysis with Morris
-outer_path = f'../output/replicates/morris/'
+outer_path = f'output/adaptive_replicates/morris/'
 cache_file = f'{outer_path}morris_level4.json'
 file_exists = os.path.exists(cache_file)
 if file_exists:
@@ -28,78 +28,83 @@ else:
     partial_order, x_morris = morris_ranking(cache_file)
     np.savetxt(outer_path + 'morris_sample.txt', x_morris)
 
-desti_folder = ['bootstrap', 'vertical', 'horizontal']
-out_path  = [f'{outer_path}{f}/' for f in desti_folder]
-for f in out_path: 
-    if not os.path.exists(f): os.makedirs(f)
-
 # Define the factor set to fix and the sample size to use
 x_fix_set = [[i for i in range(4, 21)], [i for i in range(8, 21)], 
     [i for i in range(11, 21)], [i for i in range(12, 21)], [i for i in range(15, 21)]]
 x_default = 0.25
 
 # get input samples
-r = 10 # r is the number of repetitions
-split_style = 'vertical'; size = 800; skip_numbers = 1000; 
-metric_cache = f'{out_path[0]}metric_samples.txt'
-samples_vertical = return_metric_samples(metric_cache, size, len_params, split_style, skip_numbers, r)
-metric_cache = f'{out_path[1]}metric_samples.txt'
-samples_horizontal = return_metric_samples(metric_cache, size, len_params, 'horizontal', skip_numbers, r)
+r_range = [*np.arange(10, 100, 10), *np.arange(100, 1001, 100)]
+for r in r_range:
+    print(f'--------------------- Replicate = {r}------------------------')
+    # r = 10 # r is the number of repetitions
+    desti_folder = ['bootstrap', 'vertical', 'horizontal']
+    out_path  = [f'{outer_path}{f}/r{r}/' for f in desti_folder]
+    for f in out_path: 
+        if not os.path.exists(f): os.makedirs(f)
+    
+    split_style = desti_folder[1]; size = 1000; skip_numbers = 1000; 
+    metric_cache = f'{out_path[0]}metric_samples.txt'
+    samples_vertical = return_metric_samples(metric_cache, size, len_params, split_style, skip_numbers, r)
+    metric_cache = f'{out_path[1]}metric_samples.txt'
+    samples_horizontal = return_metric_samples(metric_cache, size, len_params, desti_folder[2], skip_numbers, r)
 
-# calculation with bootstrap
-boot = True
-samples_boot = np.vstack(np.hsplit(samples_vertical, r))
-nsubsets = int(samples_boot.shape[0] / 100); nstart = 0; nboot=1000
-loop_error_metrics(out_path[0], x_fix_set, x_default, nsubsets, 1, len_params, 
-    samples_boot, evaluate, boot, file_exists, a = a, nboot = nboot, save_file=True)
+    # calculation with bootstrap
+    boot = True
+    samples_boot = np.vstack(np.hsplit(samples_vertical, r))
+    nstep = int(r * 10)
+    nsubsets = int(samples_boot.shape[0] / nstep); nstart = 0; nboot=1000
+    loop_error_metrics(out_path[0], x_fix_set, x_default, nsubsets, 1, len_params, 
+        samples_boot, evaluate, boot, file_exists, a = a, nboot = nboot, save_file=True, nstep = nstep)
 
-# calculation with replicate sampling
-boot = False
-nsubsets = int(samples_vertical.shape[0] / 10); nstart = 0; nboot=1
-loop_error_metrics(out_path[1], x_fix_set, x_default, nsubsets, r, len_params, 
-    samples_vertical, evaluate, boot, file_exists, a = a, nboot = nboot, save_file=True)
+    # calculation with replicate sampling
+    boot = False
+    nstep = 10
+    nsubsets = int(samples_vertical.shape[0] / 10); nstart = 0; nboot=1
+    loop_error_metrics(out_path[1], x_fix_set, x_default, nsubsets, r, len_params, 
+        samples_vertical, evaluate, boot, file_exists, a = a, nboot = nboot, save_file=True, nstep=nstep)
 
-loop_error_metrics(out_path[2], x_fix_set, x_default, nsubsets, r, len_params, 
-    samples_horizontal, evaluate, boot, file_exists, a = a, nboot = nboot, save_file=True)
+    loop_error_metrics(out_path[2], x_fix_set, x_default, nsubsets, r, len_params, 
+        samples_horizontal, evaluate, boot, file_exists, a = a, nboot = nboot, save_file=True, nstep=nstep)
 
-# Calculate the standard error and mean
-for f in out_path[1:]:
-    err_metrics = ['mae', 'var', 'ppmc']
-    col_names = [i + '_mean' for i in err_metrics]
-    for i in err_metrics: col_names.append(i + '_std')
-    replicates_process(f, col_names, nsubsets, r, save_file=True)
+    # Calculate the standard error and mean
+    for f in out_path[1:]:
+        err_metrics = ['mae', 'var', 'ppmc']
+        col_names = [i + '_mean' for i in err_metrics]
+        for i in err_metrics: col_names.append(i + '_std')
+        replicates_process(f, col_names, nsubsets, r, save_file=True)
 
 ##TODO clean up the following
 # Default evaluation
-defaults_list = np.append([0, 0.1, 0.2, 0.4, 0.5], np.round(np.linspace(0.21, 0.3, 10), 2))
-defaults_list.sort()
-samples_vertical  = samples_vertical[0:100, :]
-nsubsets = int(samples_vertical.shape[0] / 10)
-for x_default in defaults_list:
-    out_path = f'{outer_path}{desti_folder}/{x_default}/'
-    if not os.path.exists(out_path): os.makedirs(out_path)
-    loop_error_metrics(out_path, x_fix_set[-2:], x_default, nsubsets, r, len_params, 
-        samples_vertical, evaluate, boot, file_exists, a = a, nboot = nboot, save_file=True, nstart=9)
+# defaults_list = np.append([0, 0.1, 0.2, 0.4, 0.5], np.round(np.linspace(0.21, 0.3, 10), 2))
+# defaults_list.sort()
+# samples_vertical  = samples_vertical[0:100, :]
+# nsubsets = int(samples_vertical.shape[0] / 10)
+# for x_default in defaults_list:
+#     out_path = f'{outer_path}{desti_folder}/{x_default}/'
+#     if not os.path.exists(out_path): os.makedirs(out_path)
+#     loop_error_metrics(out_path, x_fix_set[-2:], x_default, nsubsets, r, len_params, 
+#         samples_vertical, evaluate, boot, file_exists, a = a, nboot = nboot, save_file=True, nstart=9)
 
-num_defaults = len(defaults_list)
-num_fix = ['fix_6', 'fix_9']
-df = pd.DataFrame(data = np.zeros(shape=(num_defaults, len(METRIC_NAME))), index=defaults_list,
-    columns=METRIC_NAME)
-for i in range(num_defaults):
-    replicate_collect  = np.array([])
-    out_path = f'{outer_path}sobol_vertical/{defaults_list[i]}/{num_fix[0]}/'
-    fps = os.listdir(out_path)
-    for fp in fps:
-        df_temp = pd.read_csv(out_path + fp, index_col = 'Unnamed: 0').dropna(axis=0)
-        try:
-            replicate_collect = np.vstack([replicate_collect, df_temp.iloc[0].values])
-        except ValueError:
-            replicate_collect = df_temp.iloc[0].values
+# num_defaults = len(defaults_list)
+# num_fix = ['fix_6', 'fix_9']
+# df = pd.DataFrame(data = np.zeros(shape=(num_defaults, len(METRIC_NAME))), index=defaults_list,
+#     columns=METRIC_NAME)
+# for i in range(num_defaults):
+#     replicate_collect  = np.array([])
+#     out_path = f'{outer_path}sobol_vertical/{defaults_list[i]}/{num_fix[0]}/'
+#     fps = os.listdir(out_path)
+#     for fp in fps:
+#         df_temp = pd.read_csv(out_path + fp, index_col = 'Unnamed: 0').dropna(axis=0)
+#         try:
+#             replicate_collect = np.vstack([replicate_collect, df_temp.iloc[0].values])
+#         except ValueError:
+#             replicate_collect = df_temp.iloc[0].values
 
-    for jj in range(3):
-        df.loc[defaults_list[i], METRIC_NAME[jj]], std_temp =\
-            stderr(replicate_collect[:, jj].reshape(r, 1).transpose())
-        df.loc[defaults_list[i], METRIC_NAME[jj+3]], df.loc[defaults_list[i], METRIC_NAME[jj + 6]] =\
-            df.loc[defaults_list[i], METRIC_NAME[jj]] - std_temp, df.loc[defaults_list[i], METRIC_NAME[jj]] + std_temp
-df.to_csv(f'{outer_path}sobol_vertical/{num_fix[0]}.csv')
+#     for jj in range(3):
+#         df.loc[defaults_list[i], METRIC_NAME[jj]], std_temp =\
+#             stderr(replicate_collect[:, jj].reshape(r, 1).transpose())
+#         df.loc[defaults_list[i], METRIC_NAME[jj+3]], df.loc[defaults_list[i], METRIC_NAME[jj + 6]] =\
+#             df.loc[defaults_list[i], METRIC_NAME[jj]] - std_temp, df.loc[defaults_list[i], METRIC_NAME[jj]] + std_temp
+# df.to_csv(f'{outer_path}sobol_vertical/{num_fix[0]}.csv')
     
