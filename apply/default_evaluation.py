bff79588a4af47b0c1e3c73fbbba0e4537c73528
Qian Wang <qian@hhu.edu.cn>
2021-02-26 22:04:42 +0800

    Updating default evaluation with MC sampling and bootstrap.

--- apply/default_evaluation.py
+++ apply/default_evaluation.py
@@ -6,61 +6,25 @@ from SALib.test_functions.Sobol_G import evaluate
 
 # import settings for Sobol G-function and returns necessary elements
 from utils.test_function_setting import set_sobol_g_func
-from utils.group_fix import group_fix, index_fix, results_exist
-from utils.group_fix import evaluate_wrap
-
+from utils.group_fix import loop_error_metrics
 from settings import MORRIS_DATA_DIR, METRIC_NAME
 
-a, x, x_bounds, _, _, problem = set_sobol_g_func()
-
+a, x, x_bounds, _, len_params, problem = set_sobol_g_func()
 # calculate results with fixed parameters
+# Default evaluation
 defaults_list = np.append([0, 0.1, 0.2, 0.4, 0.5], np.round(np.linspace(0.21, 0.3, 10), 2))
 defaults_list.sort()
-# defaults_list = [0.1, 0.2]
-file_exist = False
-nstart = 2400
+out_path = 'output/adaptive_replicates/morris/bootstrap_mc/'
+samples = np.loadtxt(out_path+'samples_mc.txt')[0:1000]
+nsubsets = int(samples.shape[0] / 1000)
+ind_fix_set = [[i for i in range(15, 21)], [i for i in range(12, 21)]]
+boot=True
+file_exists = False
 
-# store results from fixing parameters in dict
-error_dict = {}
-combs_fix = [[i for i in range(15, 21)], [i for i in range(12, 21)]]
-file_sample = f'../output/replicates/morris/metric_samples.csv'
-y_true_exist = True
-samples = pd.read_csv(file_sample, index_col = 'Unnamed: 0').values
-x_all = samples[:, 0:-1]
-y_true = samples[:, -1]  
-x_subset = x_all[:100]
-y_subset = y_true[:100]
-
-
-for ind_fix in combs_fix:
-    if os.path.exists(file_sample):
-        num_group = len(defaults_list) - 1
-        mae = {i: None for i in defaults_list}
-        var, ppmc = dict(mae), dict(mae)
-        mae_upper, var_upper, ppmc_upper = dict(mae), dict(mae), dict(mae)
-        mae_lower, var_lower, ppmc_lower = dict(mae), dict(mae), dict(mae)
-    for i in defaults_list:
-        pool_res = {}
-        y_fix = np.array([])
-        # loop to fix parameters and calculate the error metrics            
-        x_copy = np.copy(x_subset)
-        x_copy[:, ind_fix] = [i]
-        y_fix = evaluate_wrap(evaluate, x_copy, a)
-        y_true_ave = np.average(y_subset)
-        rand = np.random.randint(0, y_subset.shape[0], size=(1000, y_subset.shape[0]))
-        error_temp, pool_res, _ = group_fix(ind_fix, y_subset, y_fix, rand, {}, file_exist)
-        
-        [mae[i], var[i], ppmc[i], mae_lower[i], var_lower[i], ppmc_lower[i], 
-        mae_upper[i],var_upper[i], ppmc_upper[i]] = error_temp
-
-    error_dict[f'fix_{len(ind_fix)}'] = {'mae': mae, 'var': var, 'ppmc': ppmc,
-                        'mae_lower': mae_lower, 'var_lower': var_lower, 'ppmc_lower': ppmc_lower,
-                        'mae_upper': mae_upper, 'var_upper': var_upper, 'ppmc_upper': ppmc_upper}
-            # # End for
-
-# convert the result into dataframe
-for key, value in error_dict.items():
-    out_path = f'../output/replicates/morris/'
-    if not os.path.exists(out_path): os.makedirs(out_path)
-    df= pd.DataFrame(value)
-    df.to_csv(f'{out_path}/{key}.csv')
+for i in defaults_list:
+    nstep = 1000
+    nsubsets = int(samples.shape[0] / nstep); nboot=1000
+    save_path = f'{out_path}{i}/'
+    if not os.path.exists(save_path): os.mkdir(save_path)
+    loop_error_metrics(save_path, ind_fix_set, i, nsubsets, 1, len_params, 
+    samples, evaluate, boot, file_exists, a = a, nboot = nboot, save_file=True, nstep = nstep)
